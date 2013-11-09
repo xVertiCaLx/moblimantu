4f426dad2ddbfec9abf1d0166f15fdaed5cec381
wywywhy92@gmail.com <wywywhy92@gmail.com@e9864837-fea0-705e-922a-8ae2702522ce>
2013-11-09 15:11:08 +0000

    Test Test!

--- main/Search.java
+++ main/Search.java
@@ -48,7 +48,7 @@ public class Search extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("MOBLIMA App");
-        setBackground(java.awt.Color.orange);
+        setBackground(new java.awt.Color(255, 255, 255));
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         setMinimumSize(new java.awt.Dimension(630, 500));
         getContentPane().setLayout(null);
